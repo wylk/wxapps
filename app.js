1e69a5ed060c01a10b8cd778da8ed76cfc1a378d
lujiang <HR@51ao.com>
2018-02-27 16:11:36 +0800

    add 1.27日

--- app.js
+++ app.js
@@ -111,7 +111,7 @@ App({
     }, 10)
   },
   url: function (e) {
-      this.setCache("mid", {mid:110}, 7200)
+      this.setCache("mid", {mid:132}, 7200)
   },
   globalData: {
     appid: "wx0374e8ef4f1d6f8e",
